e763de6c92d4f6368050155350ac66d66dd2184e
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2023-11-27 22:11:33 -0300

    Add dependencies and configure security

---
+++ src/main/java/barberon/config/ApplicationUserDetails.java
@@ -0,0 +1,7 @@
+package barberon.config;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface ApplicationUserDetails extends UserDetails {
+}
--- src/main/java/barberon/config/SecurityConfig.java
+++ src/main/java/barberon/config/SecurityConfig.java
@@ -4,18 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
         return http.build();
     }
 }
+

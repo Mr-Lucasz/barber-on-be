07444aea88fcba98d6c6805f3821f0231df84266
BrenoJoseCoelho <93225993+BrenoJoseCoelho@users.noreply.github.com>
2023-11-28 18:37:37 -0300

    feat: adjust query agendamentos

--- src/main/java/barberon/barberonbe/repository/AgendamentoRepository.java
+++ src/main/java/barberon/barberonbe/repository/AgendamentoRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     
-    @Query("SELECT new barberon.barberonbe.DTO.RelatorioDTO(b.barbeiroNome, s.servicoTitulo, a.agendamentoHorarioInicio, s.servicoValor) " +
-           "FROM Agendamento a " +
-           "JOIN Barbeiro b ON a.agendamentoBarbeiroID = b.barbeiroID " +
-           "JOIN Servico s ON a.agendamentoServicoID = s.servicoID")
+    @Query("SELECT new barberon.barberonbe.DTO.RelatorioDTO(b.nome, s.servico_titulo, a.hora_inicio, s.servico_valor) " +
+           "FROM agendamento a " +
+           "JOIN barbeiro b ON a.barbeiro_id = b.id " +
+           "JOIN servico s ON a.servico_id = s.servico_id")
     List<RelatorioDTO> findAllRelatorio();
 }

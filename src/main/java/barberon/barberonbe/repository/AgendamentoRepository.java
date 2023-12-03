d03a105bb7a6b270f278731461477f7820c16611
Lucas.Rodrigues <Lucas.Rodrigues@ab-inbev.com>
2023-12-03 02:36:49 -0300

    Add findByClienteId and findByBarbeiroId methods to AgendamentoRepository

--- src/main/java/barberon/barberonbe/repository/AgendamentoRepository.java
+++ src/main/java/barberon/barberonbe/repository/AgendamentoRepository.java
@@ -1,12 +1,12 @@
 package barberon.barberonbe.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import barberon.barberonbe.model.Agendamento;
 import java.util.List;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
-    
+    List<Agendamento> findByClienteId(Long clienteId);
+    List<Agendamento> findByBarbeiroId(Long barbeiroId);
 }
